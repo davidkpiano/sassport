2a53239b17e3b080b247af00cab85fce28edc591
David Khourshid <davidkpiano@gmail.com>
2015-07-05 23:51:01 -0400

    Setting up tests (in progress)

---
+++ test/functions.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var should = require('should');
+
+describe('Test', function() {
+  describe('foo', function() {
+    it('should do simple math', function() {
+      assert.equal(1 + 1, 2);
+    });
+  });
+});
